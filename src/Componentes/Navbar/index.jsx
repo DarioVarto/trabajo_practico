4153bef4d0ad70898d9839c311fd5b52861282e7
DarioVarto <dariovartolo@hotmail.com>
2023-02-17 22:44:48 -0300

    "Menú responsive funcionando"

--- src/Componentes/Navbar/index.jsx
+++ src/Componentes/Navbar/index.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import {BsFacebook,BsInstagram,BsWhatsapp} from 'react-icons/bs'
 import {Logo} from '../Logo'
 import {GiHamburgerMenu} from 'react-icons/gi';
-import {MenuHamburguesa} from '../MenuHamburguesa';
+
 import { useState } from 'react';
 
  
@@ -12,33 +12,32 @@ const NavBar = () => {
 
     const [ver,setVer]= useState(false)
 
-    const mostrar = () => {
-        setVer(!ver)
-     }
+   
 
     return(
         <header className='contenedorNav'>
             
           
            <Logo/>
-         
-            <div className='contenedor_a'>
-                <a className='aNavbar' href="#Hero">Acerca de mí</a>
-                <a className='aNavbar' href="#servicios">Servicios</a>
-                <a className='aNavbar' href="#Portfolio">Portfolio</a>
-                <a className='aNavbar' href="#Opiniones">Opiniones</a>
-                <a className='aNavbar' href="#Contacto">Contacto</a>
-            </div>
-            <div className='contenedor_redes'>
+            <div className= {`nav_items ${ver && 'open'}`}>
+                <div className='contenedor_a'>
+                    <a className='aNavbar' href="#Hero">Acerca de mí</a>
+                    <a className='aNavbar' href="#servicios">Servicios</a>
+                    <a className='aNavbar' href="#Portfolio">Portfolio</a>
+                    <a className='aNavbar' href="#Opiniones">Opiniones</a>
+                    <a className='aNavbar' href="#Contacto">Contacto</a>
+                </div>
+                <div className='contenedor_redes'>
                 
-                <a href="https://www.facebook.com/profile.php?id=100090075738822" target='_blank' rel="noreferrer" className='redes_hover'><BsFacebook className='redes'/></a>
-                <a href="https://www.instagram.com/dariovarcaz/" target='_blank' rel="noreferrer" className='redes_hover'><BsInstagram className='redes'/></a>
-                <a href="https://wa.me/5491138842605/?text=Hola, quería consultarte..." target="_blank" rel="noreferrer" className='redes_hover'><BsWhatsapp className='redes'/></a>
+                    <a href="https://www.facebook.com/profile.php?id=100090075738822" target='_blank' rel="noreferrer" className='redes_hover'><BsFacebook className='redes'/></a>
+                    <a href="https://www.instagram.com/dariovarcaz/" target='_blank' rel="noreferrer" className='redes_hover'><BsInstagram className='redes'/></a>
+                    <a href="https://wa.me/5491138842605/?text=Hola, quería consultarte..." target="_blank" rel="noreferrer" className='redes_hover'><BsWhatsapp className='redes'/></a>
 
+                </div>
             </div>
-            <div className='hamburguesa'>
-                    <GiHamburgerMenu onClick={mostrar}/>
-                   {ver && <MenuHamburguesa/>}
+            <div className='hamburguesa' onClick={()=>setVer(!ver)}>
+                    <GiHamburgerMenu/>
+                     
             </div>
         </header>
     )
